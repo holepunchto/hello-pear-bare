a9f0a1d23f2aa3f6f1232d2ff58261b91454d708
Geordan Gesink <georgegeordan@gmail.com>
2026-06-09 17:13:18 +0200

    rm IPC override

--- workers/main.js
+++ workers/main.js
@@ -1,13 +1,3 @@
-const os = require('bare-os')
-const Pipe = require('bare-pipe')
-
-const channelFd = Number(os.getEnv('BARE_CHANNEL_FD'))
-const channelMode = os.getEnv('BARE_CHANNEL_SERIALIZATION_MODE')
-
-if (!Bare.IPC && Number.isFinite(channelFd) && channelMode === 'binary') {
-  Bare.IPC = new Pipe(channelFd, { ipc: true })
-}
-
 Bare.IPC.write('hello from bare worker')
 
 Bare.IPC.on('data', (data) => {
